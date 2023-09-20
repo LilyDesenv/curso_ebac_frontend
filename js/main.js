646ab9cc71ec072a568a14b9dae7ca694fd39166
LilyDesenv <alissandra.o.t@gmail.com>
2023-09-20 18:34:14 -0300

    ajuste no js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     $('#telefone').mask('(00) 00000-0000');
+
     $('#cpf').mask('000.000.000-00',{
         placeholder : "000.000.000-00"
     });
+
     $('#cep').mask('00.000-000',{
         placeholder : "00.000-000"
     });
-
-    const qntInvalido = 0;
 
     $('form').validate({
         rules: {
@@ -49,9 +49,6 @@ $(document).ready(function(){
             $("#cpf").val('');
             $("#endereco").val('');
             $("#cep").val('');
-        },
-        invalidHandler: function(evento, validador){
-
         }
     })
     
